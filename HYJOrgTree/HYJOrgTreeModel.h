e8afdbf5c390be8d0f4eabcd6b1fde05cee9fac1
hyj223 <huyujin19905@gmail.com>
2016-02-28 11:07:00 +0800

    ~

--- HYJOrgTree/HYJOrgTreeModel.h
+++ HYJOrgTree/HYJOrgTreeModel.h
@@ -21,15 +21,10 @@ typedef NS_ENUM(NSInteger, OrganizationNodeAction) {
 
 @interface HYJOrgTreeModel : NSObject
 
-@property (nonatomic, copy) NSString *businessID;   ///< 业务id
-@property (nonatomic, copy) NSString *name;         ///< 组织结构名
-@property (nonatomic, copy) NSString *type;         ///< 组织结构类型：sub & dep
-@property (nonatomic, copy) NSString *hasSubOrg;    ///< 是否有子机构
-@property (nonatomic, copy) NSString *parentDepId;  ///< 父机构ID(部门)
-@property (nonatomic, copy) NSString *parentSubId;  ///< 父机构ID(分部)
-
-@property (nonatomic, copy) NSString *parentID;     ///< 父节点（用来确定唯一的节点）
 @property (nonatomic, copy) NSString *uniqueID;     ///< ID(用来确定唯一的组织机构)
+@property (nonatomic, copy) NSString *parentID;     ///< 父节点（用来确定唯一的节点）
+@property (nonatomic, copy) NSString *name;         ///< 组织结构名
+@property (nonatomic, copy) NSString *hasSubOrg;    ///< 是否有子机构
 
 @property (nonatomic, copy) NSString *hierarchy; //层级
 @property (nonatomic, assign) OrganizationNodeState state; //结点状态~
